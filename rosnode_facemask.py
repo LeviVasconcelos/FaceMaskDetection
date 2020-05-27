72118b08bc6f5a6d94ce4fab31a67022d26ec55a
LeviVasconcelos <leviovasconcelos@gmail.com>
2020-05-27 03:09:56 +0200

    update model with global path

--- rosnode_facemask.py
+++ rosnode_facemask.py
@@ -29,7 +29,7 @@ class MaskDetector:
         self.subscriber = rospy.Subscriber("/camera/image/compressed",
                                             CompressedImage, self._callback,  queue_size = 1)
         #self.model = load_pytorch_model('models/face_mask_detection.pth');
-        self.model = load_pytorch_model('models/model360.pth');
+        self.model = load_pytorch_model('/ari_public_ws/src/FaceMaskDetection/models/model360.pth');
         # anchor configuration
         #feature_map_sizes = [[33, 33], [17, 17], [9, 9], [5, 5], [3, 3]]
         feature_map_sizes = [[45, 45], [23, 23], [12, 12], [6, 6], [4, 4]]
