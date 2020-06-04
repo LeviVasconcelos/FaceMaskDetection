9c2cac9111566a068494518643c488b747b95907
LeviVasconcelos <leviovasconcelos@gmail.com>
2020-06-04 06:21:36 +0200

    Update rosnode_facemask.py

--- rosnode_facemask.py
+++ rosnode_facemask.py
@@ -22,11 +22,11 @@ class MaskDetector:
     def __init__(self):
         '''Initialize ros publisher, ros subscriber'''
         # topic where we publish
-        self.image_pub = rospy.Publisher("/output/image_raw/compressed",
+        self.image_pub = rospy.Publisher("/output/face_mask",
                                           CompressedImage)
         # self.bridge = CvBridge()
         # subscribed Topic
-        self.subscriber = rospy.Subscriber("/camera/image/compressed",
+        self.subscriber = rospy.Subscriber("/camera_out/image",
                                             CompressedImage, self._callback,  queue_size = 1)
         #self.model = load_pytorch_model('models/face_mask_detection.pth');
         self.model = load_pytorch_model('/ari_public_ws/src/FaceMaskDetection/models/model360.pth');
